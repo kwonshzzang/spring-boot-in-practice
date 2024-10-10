d588b45f420df1a53b7458b237780a60c0989c4f
kwonshzzang <kwonshzzang@gmail.com>
2024-10-10 14:48:37 +0900

    First Commits

---
+++ chapter01/spring-boot-app-demo/src/test/java/kr/co/kwonshzzang/springbootappdemo/SpringBootAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.kwonshzzang.springbootappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
